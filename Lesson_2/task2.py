69aaaca49659db47aa1b47a80b82cea59c3d9e1d
Vladyslav Shcherbakov <shcherbakov.vladyslav@gmail.com>
2022-11-11 13:33:29 +0200

    Added first task

---
+++ Lesson_2/task2.py
@@ -0,0 +1,10 @@
+userList = list(map(str, input("Enter your list that separates by comma:\n").split(', ')))
+print("\nYour list", userList)
+
+
+
+print ("======================================")
+
+print ("======================================")
+
+print ("======================================")
